728186959171a8ffe2a6303567c334cf32378365
yogichandras <yogichandras@WIN-D9T1Q3KRDLF>
2018-03-07 15:52:10 +0700

    add

--- Program.cs
+++ Program.cs
@@ -12,14 +12,15 @@ namespace DemoApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+          public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
-    }
+
+            host.Run();
+        }
 }
